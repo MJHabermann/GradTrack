205cb5554bf70fa2f62fcc1a3402554943ae9864
Ryan Stenersen <143983140+ryansten20@users.noreply.github.com>
2025-10-06 14:23:43 -0700

    Added sign in and sign up page skeleton

--- frontend/gradtrack-app/src/App.jsx
+++ frontend/gradtrack-app/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>GradTrack</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <div className="flex-col">
+            <nav style={{ padding: "1rem", display: "flex", gap: "1rem", width: "100vw" }}>
+                <Link to="/signin">Sign In</Link>
+                <Link to="/signup">Sign Up</Link>
+            </nav>
+                <main className="flex-grow container mx-auto px-4 py-6">
+                    <Routes>
+                        <Route path="/signin" element={<Signin />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
